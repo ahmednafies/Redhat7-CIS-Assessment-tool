fdc7f3437ead419c2cb18ca85f5af0a58c77ce99
ahmednafies <ahmed.nafies@gmail.com>
2017-02-21 15:43:15 +0200

    check_discard_services_not_enabled function is added

--- services/inetdServices.py
+++ services/inetdServices.py
@@ -1,6 +1,7 @@
-
-from initialSetup import source
 import os
+
+import source
+
 
 #---------------------2.1 IN ETD SERVICES--------------------------------#
 # ---------- 2.1.1 Ensure charge services are not enabled (Scored) ---- #
@@ -18,9 +19,51 @@ def check_charge_services_not_enabled():
 
     if output1 in terminal_output:
         if output2 in terminal_output:
-           source.return_function(True,config)
+           source.return_function(True, config)
         else:
-            source.return_function(False,config)
+            source.return_function(False, config)
     else:
-        source.return_function(False,config)
+        source.return_function(False, config)
+
+#--------2.1.2 Ensure daytime services are not enabled (Scored) ---------#
+
+def check_daytime_services_not_enabled():
+
+    config = '2.1.1 Ensure daytime services are not enabled (Scored)'
+    command = 'chkconfig --list'
+    output1 = 'daytime-dgram:off'
+    output2 = 'daytime-stream:off'
+
+    print('checking "'+ config +'" ..... ')
+    terminal_variable = os.popen(command)
+    terminal_output = terminal_variable.read().replace(' ','')
+
+    if output1 in terminal_output:
+        if output2 in terminal_output:
+           source.return_function(True, config)
+        else:
+            source.return_function(False, config)
+    else:
+        source.return_function(False, config)
+
+#--------2.1.3 Ensure discard services are not enabled (Scored) ------#
+
+def check_discard_services_not_enabled():
+
+    config = '2.1.1 Ensure daytime services are not enabled (Scored)'
+    command = 'chkconfig --list'
+    output1 = 'daytime-dgram:off'
+    output2 = 'daytime-stream:off'
+
+    print('checking "'+ config +'" ..... ')
+    terminal_variable = os.popen(command)
+    terminal_output = terminal_variable.read().replace(' ','')
+
+    if output1 in terminal_output:
+        if output2 in terminal_output:
+           source.return_function(True, config)
+        else:
+            source.return_function(False, config)
+    else:
+        source.return_function(False, config)
 
--- services/services.py
+++ services/services.py
@@ -1,7 +1,9 @@
 import inetdServices
 
 def services_check():
+
     inetdServices.check_charge_services_not_enabled()
+    inetdServices.check_daytime_services_not_enabled()
 
 
 def run():
