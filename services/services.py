7755c3c4a8c16cc53a12fe7bf4c08f2ab1ab1d06
ahmednafies <ahmed.nafies@gmail.com>
2017-02-21 16:06:02 +0200

    check_time_services_not_enabled function is added

--- services/services.py
+++ services/services.py
@@ -1,9 +1,12 @@
 import inetdServices
 
-def services_check():
 
+def services_check():
     inetdServices.check_charge_services_not_enabled()
     inetdServices.check_daytime_services_not_enabled()
+    inetdServices.check_discard_services_not_enabled()
+    inetdServices.check_echo_services_not_enabled()
+    inetdServices.check_time_services_not_enabled()
 
 
 def run():
