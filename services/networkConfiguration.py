995ea13d4ad01a6f6d661f32daa084a170481b84
ahmednafies <ahmed.nafies@gmail.com>
2017-02-27 13:01:25 +0200

    network configuration check is added

---
+++ services/networkConfiguration.py
@@ -0,0 +1,34 @@
+import os
+import source
+
+# ----- NETWORK PARAMETERS -------#
+#---- 3.1.1 Ensure IP forwarding is disabled (Scored) --- #
+def check_ip_forwarding_is_disabled():
+    config = '3.1.1 Ensure IP forwarding is disabled (Scored)'
+    command = 'sysctl net.ipv4.ip_forward'
+    output = 'net.ipv4.ip_forward = 0'
+    source.output_isIn_terminal_output(config,command,output)
+
+# --- 3.1.2 Ensure packet redirect sending is disabled (Scored) --- #
+
+def check_packet_redirect_sending_is_disabled():
+    config = '3.1.2 Ensure packet redirect sending is disabled (Scored)'
+
+    command1 = 'sysctl net.ipv4.conf.all.send_redirects'
+    output1 = 'net.ipv4.conf.all.send_redirects = 0'
+    command2 = 'sysctl net.ipv4.conf.default.send_redirects'
+    output2 = 'net.ipv4.conf.default.send_redirects = 0'
+
+    print('checking "' + config + '" ..... ')
+
+    terminal_variable = os.popen(command1)
+    terminal_output1 = terminal_variable.read()
+
+    terminal_variable = os.popen(command2)
+    terminal_output2 = terminal_variable.read()
+
+    if output1 in terminal_output1 and output2 in terminal_output2:
+        source.return_function(True, config)
+    else:
+        source.return_function(False, config)
+
--- services/services.py
+++ services/services.py
@@ -1,6 +1,7 @@
 import inetdServices
 import timeSynchronization
 import serviceClients
+import networkConfiguration
 
 def services_check():
     inetdServices.check_charge_services_not_enabled()
@@ -43,9 +44,15 @@ def service_clients_check():
     serviceClients.check_telnet_client_not_installed()
     serviceClients.check_LDAP_client_not_installed()
 
+def network_configuration_check():
+    networkConfiguration.check_ip_forwarding_is_disabled()
+    networkConfiguration.check_packet_redirect_sending_is_disabled()
+
 def run():
 #    services_check()
 #    special_purpose_services_check()
-    service_clients_check()
+#    service_clients_check()
+    network_configuration_check()
+
 
 run()
